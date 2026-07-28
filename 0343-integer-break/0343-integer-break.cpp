3f897bdfdecf6e6354b82bd4804a10d6dee1b4d1
yash00212 <yashmaurya242@gmail.com>
2026-07-28 09:33:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (91.7%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+        
+        int ans = 1;
+        while(n>4){
+            ans = ans * 3;
+            n = n - 3;
+        }
+        ans = ans * n;
+        return ans;
+    }
+};
